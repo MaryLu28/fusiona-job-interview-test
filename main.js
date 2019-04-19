5eb6ff4199fd8d674159fe4a53ded117a04a5f33
María Lourdes Garcia Flórez <mgarcia@turpialdev.com>
2019-04-19 02:35:15 -0400

    scroll with navbar completed

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
-var miTitulo = document.querySelector('h1');
-miTitulo.innerHTML = 'Hello world!';
+window.onscroll = function () {
+  scrollFunction()
+};
+
+/*  */
+function scrollFunction() {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    document.getElementById("navbar").className = "navbar-fixed";
+  } else {
+    document.getElementById("navbar").className = "";
+  }
+}
+
+/*  */
+var btns = document.getElementsByClassName("navbar-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+/*  */
+/* function setURL() {
+  var url = window.location.href.split("#");
+  var newurl = "/" + url[0] + "/" + url[1];
+  window.location.href = newurl
+} */
